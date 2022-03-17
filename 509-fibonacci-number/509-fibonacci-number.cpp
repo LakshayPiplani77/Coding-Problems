0b601e41e2ad5e2a70b42e009b2a1e6403d3110b
LakshayPiplani77 <100138503+LakshayPiplani77@users.noreply.github.com>
2022-03-17 15:46:19 +0530

    Time: 4 ms (52.43%), Space: 5.7 MB (96.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
-    int fib_helper(int n,int *ans)
-{
-  if(n<=1)
-  return n;
-  //check if output already exists
-  if(ans[n] != -1)
-  {
-  return ans[n];
-}
-int a = fib_helper(n-1,ans);
-int b = fib_helper(n-2,ans);
-ans[n] = a+b; //save for future use
-return ans[n]; //return the final output
-}
-int fib(int n)
-{
-  int *ans = new int[n+1];
-  for(int i = 0;i<=n;i++)
-  {
-    ans[i] = -1;
-  }
-  return fib_helper(n,ans);
-}
+	int fib(int N) {
+		if(N < 2){
+			return N;
+		}
+		int dp[N + 1];
+		dp[0] = 0;
+		dp[1] = 1;
+		for(int i = 2; i <= N; i++){
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		return dp[N];
+	}
 };
