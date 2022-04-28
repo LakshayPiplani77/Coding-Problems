70af9b1a85ae64ccdfbf95a760a03367b68e2afb
LakshayPiplani77 <100138503+LakshayPiplani77@users.noreply.github.com>
2022-04-28 14:00:19 +0530

    Added solution - LeetHub

---
+++ 30. Logical Operators - GFG/30.-logical-operators.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void logicOp(int a, int b);
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+
+void logicOp(int a, int b)
+{
+    /*output (a&&b), (a||b), and ((!a)&&(!b))separated by spaces*/ 
+    bool x = a && b;
+    bool y = a || b;
+    bool z = ((!a) && (!b));
+    cout<<x<<" "<<y<<" "<<z;
+    cout<<endl;
+}
+// { Driver Code Starts.
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int a,b;
+	    cin>>a>>b;
+	    logicOp(a,b);
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
