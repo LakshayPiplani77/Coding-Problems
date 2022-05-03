0248b83657fd676cb8df22b19a553861dd0ec492
LakshayPiplani77 <100138503+LakshayPiplani77@users.noreply.github.com>
2022-05-03 11:40:17 +0530

    Time: 32 ms (83.37%), Space: 28.8 MB (6.92%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     int findUnsortedSubarray(vector<int>& nums) {
-
-        int ans = 0;  
-        
-        // Two pointers
-        int i = 0;
-        int j = nums.size() - 1;
-
-        int curr_max=INT_MAX;
-        int curr_min=INT_MIN;
-        
-        // Initialize end to -1, if we have all sorted in ascending order, then end-start+1 = -1+0+1 = 0 
-        int start=0,end=-1;
-        
-        while(j >= 0){
-            // Check if the "num[start]" is greater than the minimum element so far
-            if(nums[i] >= curr_min) curr_min = nums[i];
-            else end = i; // m contains the 
-
-            // Check if the "num[end]" is less than the maximum element so far
-            if(nums[j] <= curr_max) curr_max = nums[j];
-            else start = j;
-
-            ++i;
-            --j;
+        vector<int> temp;
+        for(int i = 0;i<nums.size();i++)
+        {
+            temp.push_back(nums[i]);
         }
-        return end - start + 1;
+        sort(temp.begin(),temp.end());
+        int start = 0;
+        while(start < nums.size() && nums[start] == temp[start])
+        {
+            start++;
+        }
+        int end = nums.size()-1;
+        while(end >= 0 && nums[end] == temp[end])
+        {
+            end--;
+        }
+        if(start<end)
+            return end-start+1;
+        else
+            return 0;
     }
 };
