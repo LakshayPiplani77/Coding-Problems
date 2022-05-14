8b07f11074ae287e8111a96725bea8181710c871
LakshayPiplani77 <100138503+LakshayPiplani77@users.noreply.github.com>
2022-05-14 20:28:36 +0530

    Added solution - LeetHub

---
+++ 3. Count 1's in binary array - GFG/3.-count-1s-in-binary-array.c
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+// Function to count number of Ones
+// arr: input array 
+// N: size of input array
+int countOnes(int arr[], int N)
+{
+    //Your code here
+    int start = 0;
+    int end = N-1;
+    while(start<=end)
+    {
+        int mid = start + (end-start)/2;
+        if(arr[mid] == 0){
+            end = mid-1;
+        }
+        else
+        {
+        if(mid == N-1 || arr[mid+1] == 0)
+        return mid+1;
+        else
+        start = mid+1;
+        }
+    
+    }
+}
+
+// { Driver Code Starts.
+
+int main(){
+    
+    int t;
+	scanf("%d", &t);
+	while (t--)
+	{
+		int n;
+		scanf("%d", &n);
+		int *a;
+		a = (int*)malloc(n * sizeof(int));
+		for (int i = 0; i < n; ++i)
+			scanf("%d", &a[i]);
+
+		printf("%d\n", countOnes(a, n) );
+	}
+	return 0;
+	
+}
+  // } Driver Code Ends
