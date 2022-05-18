6a6c9e20df47c5eb4314d2e3b9941826b5784b81
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-05-18 10:54:22 +0530

    Added solution - LeetHub

--- 4. Square root of a number - GFG/4.-square-root-of-a-number.cpp
+++ 4. Square root of a number - GFG/4.-square-root-of-a-number.cpp
@@ -20,9 +20,8 @@ class Solution{
             return mid;
             else if(mid*mid > x)
             end = mid-1;
-            else if(mid*mid < x) {
+            else if(mid*mid < x)
             start = mid + 1;
-            }
             mid = start + (end-start)/2;
         }
         return end;
