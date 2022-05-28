91aa9c2a12b700ad6e4f5a0d489fafcd24591bb1
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-05-28 12:43:07 +0530

    Added solution - LeetHub

--- 7. Primality Test - GFG/7.-primality-test.cpp
+++ 7. Primality Test - GFG/7.-primality-test.cpp
@@ -13,13 +13,34 @@ class Solution{
        bool isPrime(int N)
     {
         //Your code here
-        for(int i = 2;i*i<= N;i++)
+        if(N == 1)
+        return false;
+        if(N == 2 || N == 3)
+        return true;
+        if(N%2 == 0 || N%3 == 0)
+        return false;
+        for(int i = 5;i*i<=N;i+=6)
         {
-            if(N%i == 0)
+            if(N%i == 0 || N%(i+2) == 0) 
             return false;
         }
         return true;
     }
+    
+    //   bool isPrime(int N)
+    // {
+    //     //Your code here
+    //     for(int i = 2;i*i<= N;i++)
+    //     {
+    //         if(N%i == 0)
+    //         return false;
+    //     }
+    //     return true;
+    // }
+    
+    
+    
+    
     //     bool isPrime(int N)
     // {
     //     //Your code here
