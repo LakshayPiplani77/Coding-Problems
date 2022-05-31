40b7bf99600145bda83a591be192e1eaaf1ba86f
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-05-31 19:50:09 +0530

    Added solution - LeetHub

--- 2. Peak element - GFG/2.-peak-element.cpp
+++ 2. Peak element - GFG/2.-peak-element.cpp
@@ -12,22 +12,33 @@ using namespace std;
 class Solution
 {
     public:
-    int peakElement(int arr[], int n)
-    {
-       // Your code here
-       if(n == 1)
-  return 0;
-  if(arr[0] >= arr[1] )
-  return 0;
-  if(arr[n-1] > arr[n-2])
-  return n-1;
-  for(int i = 1;i<n-1;i++)
-  {
-    if(arr[i] >= arr[i-1] && arr[i] >= arr[i+1])
-    return i;
-  }
- 
+int findPeakUtil(int arr[], int low, int high, int n)
+{
+    int l = low;
+    int r = high;
+    int mid;
+    while (l <= r) {
+        // finding mid by binary right shifting.
+        mid = (l + r) >> 1;
+        // first case if mid is the answer
+        if ((mid == 0 || arr[mid - 1] <= arr[mid])
+            and (mid == n - 1 || arr[mid + 1] <= arr[mid]))
+            break;
+        // if we have to perform left recursion
+        if (mid > 0 and arr[mid - 1] > arr[mid])
+            r = mid - 1;
+        // else right recursion.
+        else
+            l = mid + 1;
     }
+    return mid;
+}
+ 
+// A wrapper over recursive function findPeakUtil()
+int peakElement(int arr[], int n)
+{
+    return findPeakUtil(arr, 0, n - 1, n);
+}
 };
 
 // { Driver Code Starts.
