ec861d463dd91f0605ae90af50451d574b95c263
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-05-31 20:05:42 +0530

    Added solution - LeetHub

---
+++ 1. Left Index - GFG/1.-left-index.cpp
@@ -0,0 +1,55 @@
+// { Driver Code Starts
+#include <iostream>
+using namespace std;
+
+
+ // } Driver Code Ends
+// Function to find element in sorted array
+
+int leftIndex(int N, int arr[], int X){
+    
+    // Your code here
+    int ans = -1;
+    for(int i = 0;i<N;i++)
+    {
+        if(arr[i] == X)
+        {
+            ans = i;
+            break;
+        }
+    }
+    return ans;
+}
+
+
+
+
+// { Driver Code Starts.
+
+// Driver Code
+int main() {
+	
+	// Testcase input
+	int testcases;
+	cin >> testcases;
+    
+    // Looping through all testcases
+	while(testcases--){
+	    int sizeOfArray;
+	    cin >> sizeOfArray;
+	    
+	    int arr[sizeOfArray];
+	    
+	    // Array input
+	    for(int index = 0; index < sizeOfArray; index++){
+	        cin >> arr[index];
+	    }
+	    
+	    int elemntToSearch;
+	    cin >> elemntToSearch;
+	    
+	    cout << leftIndex(sizeOfArray, arr, elemntToSearch) << endl;
+	}
+	
+	return 0;
+}  // } Driver Code Ends
