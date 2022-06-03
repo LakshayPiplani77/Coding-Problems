d13ec6115dfc4f3118e46ab6d721a12fa1e0d615
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-06-03 20:45:09 +0530

    Added solution - LeetHub

---
+++ 5. For Loop 2- Python - GFG/5.-for-loop-2-python.py
@@ -0,0 +1,33 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+ # } Driver Code Ends
+#User function Template for python3
+
+
+def stringJumper(str):
+    b = len(str)
+    for i in range(0,b,2): ## from 0 to length of str and skip 2
+        print(str[i], end="") ##printing character and separating characters by nothing
+
+
+
+#{ 
+#Driver Code Starts.
+
+
+
+def main():
+    testcases = int(input()) #testcases
+    while(testcases>0):
+        str = (input())
+        stringJumper(str)
+        print()##separating testcases outputs by newlines
+        testcases -= 1
+        
+
+
+if __name__=='__main__':
+    main()
+#} Driver Code Ends
