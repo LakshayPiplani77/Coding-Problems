077b06c3240686c621dd0fffb524db184108255c
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-06-03 15:10:23 +0530

    Added solution - LeetHub

--- 5. Quick Sort - GFG/5.-quick-sort.cpp
+++ 5. Quick Sort - GFG/5.-quick-sort.cpp
@@ -26,25 +26,49 @@ class Solution
  
         // Separately sort elements before
         // partition and after partition
-        quickSort(arr, low, pi - 1);
+        quickSort(arr, low, pi);
         quickSort(arr, pi + 1, high);
     }
 }
     public:
-int partition (int arr[], int low, int high)
+int partition(int arr[], int low, int high)
 {
-  int pivot = arr[high];
-  int i = low-1;
-  for(int j = low;j<=high-1;j++)
-  {
-      if(arr[j] < pivot)
-      {
-          i++;
-          swap(arr[i],arr[j]);
-      }
-  }
-  swap(arr[i+1],arr[high]);
-  return i+1;
+    int pivot = arr[low];
+    int i = low - 1, j = high + 1;
+ 
+    while (true) {
+        // Find leftmost element greater than
+        // or equal to pivot
+        do {
+            i++;
+        } while (arr[i] < pivot);
+ 
+        // Find rightmost element smaller than
+        // or equal to pivot
+        do {
+            j--;
+        } while (arr[j] > pivot);
+ 
+        // If two pointers met.
+        if (i >= j)
+            return j;
+ 
+        swap(arr[i], arr[j]);
+}
+    
+    //LOMUTO's partition
+//   int pivot = arr[high];
+//   int i = low-1;
+//   for(int j = low;j<=high-1;j++)
+//   {
+//       if(arr[j] < pivot)
+//       {
+//           i++;
+//           swap(arr[i],arr[j]);
+//       }
+//   }
+//   swap(arr[i+1],arr[high]);
+//   return i+1;
 }
 };
 
