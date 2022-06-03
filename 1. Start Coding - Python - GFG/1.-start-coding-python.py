69e8a4286ae657133c3e5bb3f40cb94026c18e45
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-06-03 18:31:36 +0530

    Added solution - LeetHub

---
+++ 1. Start Coding - Python - GFG/1.-start-coding-python.py
@@ -0,0 +1,21 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+
+
+ # } Driver Code Ends
+#User function Template for python3
+
+def print_fun():
+    print("Hello World")
+
+#{ 
+#Driver Code Starts.
+
+def main():
+    print_fun()
+
+if __name__ == '__main__':
+    main()
+#} Driver Code Ends
