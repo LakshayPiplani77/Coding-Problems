9fc5d7c1df4527f0139e2cb17a87301687965572
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-06-09 15:22:10 +0530

    Added solution - LeetHub

--- 3. Count nodes of linked list - GFG/3.-count-nodes-of-linked-list.cpp
+++ 3. Count nodes of linked list - GFG/3.-count-nodes-of-linked-list.cpp
@@ -30,19 +30,16 @@ class Solution
     //Function to count nodes of a linked list.
     int getCount(struct Node* head){
     
-      int count = 0;
-      if(head == NULL)
-         return 0;
-      if(head->next == NULL)
-         return 1;
-      Node* temp = head;
-      while(temp != NULL)
-      {
-          count++;
-          temp = temp->next;
-      }
-      return count;
-    
+        //Code here
+        int count = 0;
+        Node* temp = head;
+        while(temp != NULL)
+        {
+            count++;
+            temp = temp->next;
+        }
+        
+    return count;
     }
 };
     
