b665de748ee897b9023bba8fc6d374e00723765d
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-06-22 13:38:50 +0530

    Added solution - LeetHub

---
+++ 1. Forward List Insert - GFG/1.-forward-list-insert.cpp
@@ -0,0 +1,62 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+// arr[]: insert the elements of array to forwardList
+// n: is the size of array
+//Complete the function
+forward_list<int> insertIntoFL(int arr[],int n)
+{
+    //your code here
+    forward_list<int> ans;
+    //Use push_front() method to push the elements in a forward list and return the list
+    for(int i = 0;i<n;i++)
+    {
+        ans.push_front(arr[i]);
+    }
+    return ans;
+}
+
+
+// { Driver Code Starts.
+
+
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--)
+	{
+	    int n;
+	    cin>>n;
+	    
+	    int arr[n];
+	    
+	    
+	    for(int i=0;i<n;i++)
+	    {
+	        int x;
+	        cin>>x;
+	       arr[i]=x;
+	    }
+	    
+	    forward_list<int> temp=insertIntoFL(arr,n);
+	    
+	    for(auto i:temp)
+	    cout<<i<<" ";
+	    
+	    cout<<endl;
+	    
+	    
+	    
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
