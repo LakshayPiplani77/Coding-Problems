0056c39861fc7db2b0d5e65ca01c9784a7fda916
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-06-22 21:22:12 +0530

    Time: 3 ms (43.88%), Space: 6.3 MB (50.88%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.cpp
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -1,47 +1,34 @@
-#include<stack>
 class Solution {
 public:
     bool isValid(string s) {
-     stack<char> st;
-    char x;
-    for(int i = 0;i<s.length();i++)
-    {
-        if(s[i] == '(' || s[i] == '[' || s[i] == '{')
+        stack<char> st;
+        for(int i = 0;i<s.size();i++)
         {
-            st.push(s[i]);
-            continue;
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[')
+            {
+                st.push(s[i]);
+            }
+            else{
+                if(!st.empty())
+                {
+                    if(s[i] == ')' && st.top() == '(' || s[i] == '}' && st.top() == '{' || s[i] == ']' && st.top() == '[')
+                    {
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                }
+                }
+                else{
+                    return false;
+                }
+                
+                
+            }
         }
         if(st.empty())
-            return false;
-        switch (s[i]) {
-        case ')':
-             
-            // Store the top element in a
-            x = st.top();
-            st.pop();
-            if (x == '{' || x == '[')
-                return false;
-            break;
- 
-        case '}':
- 
-            // Store the top element in b
-            x = st.top();
-            st.pop();
-            if (x == '(' || x == '[')
-                return false;
-            break;
- 
-        case ']':
- 
-            // Store the top element in c
-            x = st.top();
-            st.pop();
-            if (x == '(' || x == '{')
-                return false;
-            break;
-        }
-    }
-    return (st.empty());   
+        return true;
+        else
+        return false;
     }
 };
