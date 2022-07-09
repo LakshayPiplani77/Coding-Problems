848f7024436fd35b6d29e835c3b73223915f112c
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-07-09 17:09:07 +0530

    Added solution - LeetHub

--- Palindrome String - GFG/palindrome-string.cpp
+++ Palindrome String - GFG/palindrome-string.cpp
@@ -9,19 +9,21 @@ using namespace std;
 class Solution{
 public:	
 	
-	
+	int check(string &s, int start,int end)
+	{
+	    if(s.size() == 0 || s.size() == 1)
+	    return 1;
+	    if(start >= end)
+	    return 1;
+	    if(s[start] != s[end])
+	    return 0;
+	    return check(s,start+1,end-1);
+	}
 	int isPalindrome(string S)
 	{
 	    // Your code goes here
-	    int n = S.size();
-	    int i = 0;
-	    while(i <= S.length()/2)
-	    {
-	        if(S[i] != S[n-i-1])
-	        return 0;
-	        i++;
-	    }
-	    return 1;
+	    int ans = check(S,0,S.length()-1);
+	    return ans;
 	}
 
 };
