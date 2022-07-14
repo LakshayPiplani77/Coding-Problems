e31ec3064bf34981de7addc32dce39bdb377a79c
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-07-14 11:06:49 +0530

    Time: 113 ms (8.96%), Space: 74.5 MB (10.31%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -11,32 +11,30 @@
  */
 class Solution {
 private:
-    void solve(TreeNode* root,int targetSum,int &count,vector<int> path)
+    void solve(TreeNode* root,int k,vector<int> path,int &count)
     {
-        if(root == NULL){
+        if(root == NULL)
             return;
-        }
         path.push_back(root->val);
-        solve(root->left,targetSum,count,path);
-        solve(root->right,targetSum,count,path);
+        //left
+        solve(root->left,k,path,count);
+        //right
+        solve(root->right,k,path,count);
+        int s = path.size();
         long sum = 0;
-        for(int i = path.size()-1;i>=0;i--)
+        for(int i = s-1;i>=0;i--)
         {
             sum += path[i];
-            if(sum == targetSum)
-            {
+            if(sum == k)
                 count++;
-            }
         }
         path.pop_back();
-
     }
-    
 public:
     int pathSum(TreeNode* root, int targetSum) {
         vector<int> path;
         int count = 0;
-        solve(root,targetSum,count,path);
+        solve(root,targetSum,path,count);
         return count;
     }
 };
