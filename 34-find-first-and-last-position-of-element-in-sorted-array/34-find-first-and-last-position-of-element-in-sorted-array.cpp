eddb670f3a931dbe8c4ccaa37f83e0560e03ab8a
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-07-24 13:22:57 +0530

    Time: 19 ms (18.33%), Space: 13.6 MB (83.66%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,53 +1,60 @@
 class Solution {
+private:
+    int firstOcc(vector<int> &nums,int target)
+    {
+        int start = 0;
+        int end = nums.size()-1;
+        int ans = -1;
+        while(start <= end)
+        {
+            int mid = start +  (end-start)/2;
+            if(nums[mid] == target)
+            {
+                ans = mid;
+                end = mid-1;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
+     int lastOcc(vector<int> &nums,int target)
+    {
+        int start = 0;
+        int end = nums.size()-1;
+        int ans = -1;
+        while(start <= end)
+        {
+            int mid = start +  (end-start)/2;
+            if(nums[mid] == target)
+            {
+                ans = mid;
+                start = mid+1;
+            }
+            else if(nums[mid] < target)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
+            }
+        }
+        return ans;
+    }
 public:
-    int lowerBound(vector<int>& nums, int target){
-        
-        int n = nums.size();
-        int i=0,j=n-1;
-        int ans = -1;
-        int mid;
-        while(i <= j){
-            mid = i + ((j-i)/2);
-            
-            if(nums[mid] < target){
-                i = mid+1;
-            }else if(nums[mid] > target){
-                j = mid-1;
-            }else{
-                ans = mid;
-                j = mid-1;
-            }
-        }
-        return ans;
-    }    
-    
-    int upperBound(vector<int>& nums, int target){
-        
-        int n = nums.size();
-        int i=0,j=n-1;
-        
-        int ans = -1;
-        int mid;
-        while(i <= j){
-            mid = i + ((j-i)/2);
-            
-            if(nums[mid] < target){
-                i = mid+1;
-            }else if(nums[mid] > target){
-                j = mid-1;
-            }else{
-                ans = mid;
-                i = mid+1;
-            }
-        }
-        return ans;
-    }    
-    
     vector<int> searchRange(vector<int>& nums, int target) {
-        
-        int lb = lowerBound(nums,target);
-        int ub = upperBound(nums,target);
-        
-        return {lb,ub};
+        vector<int> ans;
+        int first = firstOcc(nums,target);
+        int last = lastOcc(nums,target);
+        ans.push_back(first);
+        ans.push_back(last);
+        return ans;
     }
 };
