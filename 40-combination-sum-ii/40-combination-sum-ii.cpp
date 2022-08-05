66c52e38612761064b156cf72742e54348ac20b6
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-05 09:28:58 +0530

    Time: 7 ms (76.83%), Space: 10.6 MB (73.59%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -9,21 +9,20 @@ private:
         }
         for(int i = index;i<arr.size();i++)
         {
-            if(i > index && arr[i] == arr[i-1])
-                continue;
-            if(arr[i]> target)
-                break;
+            if(i > index && arr[i] == arr[i-1]) continue; 
+            if(arr[i] > target) break;
             ds.push_back(arr[i]);
             solve(i+1,target-arr[i],arr,ans,ds);
             ds.pop_back();
+            
         }
     }
 public:
-    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        sort(candidates.begin(),candidates.end());
+    vector<vector<int>> combinationSum2(vector<int>& arr, int target) {
+        sort(arr.begin(),arr.end());
         vector<vector<int>> ans;
         vector<int> ds;
-        solve(0,target,candidates,ans,ds);
+        solve(0,target,arr,ans,ds);
         return ans;
     }
 };
