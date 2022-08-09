8508a4e7d5a1788e0a6100d6fcd36d192760ec4f
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-09 21:42:50 +0530

    Time: 619 ms (5.04%), Space: 20 MB (25.52%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+vector<vector<int>> merge(vector<vector<int>>& intervals) {
+sort(intervals.begin(),intervals.end());//O(NlogN)
+vector<vector<int>> ans;
+for(int i=0;i<intervals.size();i++)//O(N^2)
+{
+int start=intervals[i][0];//1
+int end=intervals[i][1];//3
+//if the vector is alreay present in the data structure we are not gonna push it again//
+if(ans.empty()==false){
+if(start<=ans.back()[1])
+{
+continue;
+}}
+for(int j=i+1;j<intervals.size();j++)
+{
+if(intervals[j][0]<=end)
+{
+end=max(end,intervals[j][1]);
+}
+}//O(NlogN)
+vector<int> res;
+res.push_back(start);
+res.push_back(end);
+ans.push_back(res);
+}
+return ans;
+}
+};
