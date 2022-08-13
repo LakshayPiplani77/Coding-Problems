b8690cbfb62546aab2651db7918d4d497655c290
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-13 16:48:57 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (39.89%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int fib(int n) {
-        if(n <= 1)
+        if(n <=1)
             return n;
-        vector<int> dp(n+1,0);
-        dp[0] = 0;
-        dp[1] = 1;
+        int prev2 = 0,prev = 1;
         for(int i = 2;i<=n;i++)
         {
-            dp[i] = dp[i-1] + dp[i-2];
+            int curri = prev + prev2;
+            prev2 = prev;
+            prev = curri;
+            
         }
-        return dp[n];
+        return prev;
     }
 };
