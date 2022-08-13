ce45e561b13bbc994e74d3427cf0808e29721e98
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-13 17:05:17 +0530

    Time: 2 ms (42.60%), Space: 6.3 MB (15.55%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,20 +1,14 @@
 class Solution {
-private:
-    int climbStairsHelper(int n,vector<int> &dp)
-    {
-        if(n == 0)
-            return 1;
-        if(n == 1)
-            return 1;
-        if(dp[n] != -1)
-            return dp[n];
-        int left = climbStairsHelper(n-1,dp);
-        int right = climbStairsHelper(n-2,dp);
-        return dp[n] = left + right;
-    }
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return climbStairsHelper(n,dp);
+        if(n <= 1)
+            return 1;
+        vector<int> dp(n+1,0);
+        dp[0] = 1,dp[1] = 1;
+        for(int i = 2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
