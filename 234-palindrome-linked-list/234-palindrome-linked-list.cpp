9ef5767ccaa912dc90cdd43b9f5151349e6c84a5
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-14 00:12:42 +0530

    Time: 348 ms (57.85%), Space: 118 MB (49.46%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -1,37 +1,62 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-private:
-    bool checkPalin(vector<int> &ll)
-    {
-        int start = 0;
-        int end = ll.size()-1;
-        while(start < end)
-        {
-            if(ll[start] != ll[end])
-                return false;
-            start++;
-            end--;
+class Solution{
+  private:
+    ListNode* getMid(ListNode* head ) {
+        ListNode* slow = head;
+        ListNode* fast = head -> next;
+        
+        while(fast != NULL && fast-> next != NULL) {
+            fast = fast -> next -> next;
+            slow = slow -> next;
         }
-        return true;
+        
+        return slow;
     }
-public:
-    bool isPalindrome(ListNode* head) {
-        vector<int> ll;
-        ListNode* temp = head;
-        while(temp != NULL)
-        {
-            ll.push_back(temp->val);
-            temp = temp->next;
+    ListNode* reverse(ListNode* head) {
+        
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+        ListNode* next = NULL;
+        
+        while(curr != NULL) {
+            next = curr -> next;
+            curr -> next = prev;
+            prev = curr;
+            curr = next;
         }
-        return checkPalin(ll);
+        return prev;
+    }
+  public:
+    //Function to check whether the list is palindrome.
+    bool isPalindrome(ListNode *head)
+    {
+        if(head -> next == NULL) {
+            return true;
+        }
+        
+        //step 1 -> find Middle
+        ListNode* middle = getMid(head);
+        //cout << "Middle " << middle->data << endl;
+        
+        //step2 -> reverse List after Middle
+        ListNode* temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        //step3 - Compare both halves
+        ListNode* head1 = head;
+        ListNode* head2 = middle -> next;
+        
+        while(head2 != NULL) {
+            if(head2->val != head1->val) {
+                return 0;
+            }
+            head1 = head1 -> next;
+            head2 = head2 -> next;
+        }
+    
+        //step4 - repeat step 2
+        temp = middle -> next;
+        middle -> next = reverse(temp);
+        
+        return true;
     }
 };
