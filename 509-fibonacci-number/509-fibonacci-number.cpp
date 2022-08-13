58732ffbacf2976cb7ac297885b49a1432a6e948
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-13 16:46:09 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.75%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,21 +1,15 @@
 class Solution {
-private:
-    int fiboHelper(int n,vector<int> &dp)
-    {
-        if(n<=1)
-        {
-            return n;
-            
-        }
-        if(dp[n] != -1)
-        {
-            return dp[n];
-        }
-        return dp[n] = fiboHelper(n-1,dp) + fiboHelper(n-2,dp);
-    }
 public:
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return fiboHelper(n,dp);
+        if(n <= 1)
+            return n;
+        vector<int> dp(n+1,0);
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2;i<=n;i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
     }
 };
