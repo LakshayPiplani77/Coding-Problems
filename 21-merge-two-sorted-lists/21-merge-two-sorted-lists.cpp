ecaf667be3243bfc6102bdb05e765d75bbede85f
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-14 21:38:11 +0530

    Time: 14 ms (42.63%), Space: 14.7 MB (97.83%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -1,24 +1,46 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-class Solution {
-public:
-   ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if(l1 == NULL) return l2;
-        if(l2 == NULL) return l1;
-        if(l1->val>=l2->val) 
-            l2->next = mergeTwoLists(l1, l2-> next);
-        else{
-            l1->next = mergeTwoLists(l1->next, l2);
-            l2 = l1;
+class Solution{
+private:
+ListNode * solve(ListNode* first, ListNode* second){
+        if(first->next == NULL) 
+        {first->next =  second;
+        return first;}
+        ListNode* cur1=first;
+        ListNode* cur2 = second;
+        ListNode* next1 = first->next;
+        ListNode* next2 = second->next;
+        
+        
+        while(next1 != NULL && cur2 != NULL){
+            if((cur2->val >= cur1->val) && (cur2->val<=next1->val)){
+                
+                cur1->next = cur2;
+                next2 = cur2->next;
+                cur2->next = next1;
+                
+                cur1= cur2;
+                cur2 = next2;
+            }
+            else {
+                cur1=cur1->next;
+                next1=next1->next;
+                
+                if(next1==NULL){
+                    cur1->next = cur2;
+                    return first;
+                }
+            }
         }
-       return l2;
+        return first;
+    }
+    public:
+    
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        if(list1 == NULL) return list2;
+        if(list2 == NULL) return list1;
+        
+        if(list1->val <= list2->val) 
+            return solve(list1,list2);
+        else 
+            return solve(list2,list1);
     }
 };
