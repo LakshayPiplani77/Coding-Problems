8bf31edf6f5443c0204ae5112e35bd4ef3a98b5b
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-17 00:01:06 +0530

    Time: 10 ms (43.14%), Space: 5.9 MB (70.22%) - LeetHub

---
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int arrangeCoins(int n) {
+        int count = 0;
+        int i = 1;
+        while(n > 0)
+        {
+            n = n-i;
+            i++;
+            if(n < 0)
+                break;
+            count++;
+        }
+        return count;
+    }
+};
