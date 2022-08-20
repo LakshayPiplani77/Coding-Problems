30781a4baef045e81372e3f684db0310cc8fa53d
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-20 13:26:29 +0530

    Time: 3 ms (88.69%), Space: 11.1 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -1,43 +1,28 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low = 0;
-        int high = nums.size() - 1;
-        
-        while(low <= high){
-            int mid = low + (high - low) / 2;
-            
-            if(nums[mid] == target){
+        int low = 0, high = nums.size()-1;
+        while(low <= high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid] == target)
                 return mid;
+            // left side is sorted
+            if(nums[low] <= nums[mid])
+            {
+                if(target >= nums[low] && target <= nums[mid])
+                    high = mid-1;
+                else 
+                    low = mid+1;
             }
-            
-            // if low is less than mid (not less or equal because equal is handled by the previous if statement), then low to mid is sorted
-            if (nums[low] <= nums[mid]){
-                // if target is between low to mid,
-                // narrow search down to low to mid
-                if(target >= nums[low] and target <= nums[mid]){
-                    high = mid;
-                } else {
-                    // if target is beyond mid
-                    // narrow search down to mid + 1 to high
-                    low = mid + 1;
-                }
-                // if low is greater than mid,
-                // then we're in the rotated part
-            } else if (nums[low] > nums[mid]){
-                // if target is between mid to high
-                // narrow search down to mid to high
-                if(target >= nums[mid] and target <= nums[high]){
-                    low = mid + 1;
-                } else {
-                    // if target is greater than high and mid both
-                    // narrow search down to the second part before
-                    // mid. So low to mid.
-                    high = mid;
-                }
+            else
+            {
+                if(target >= nums[mid] && target <= nums[high])
+                    low = mid+1;
+                else
+                    high = mid-1;
             }
         }
-        
         return -1;
     }
 };
