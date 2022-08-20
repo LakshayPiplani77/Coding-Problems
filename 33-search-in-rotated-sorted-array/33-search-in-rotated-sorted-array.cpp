b0999bc7cbdc6f25362b16111670b5e08d904b2a
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-20 13:34:55 +0530

    Time: 3 ms (88.69%), Space: 11.1 MB (29.48%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -5,24 +5,36 @@ public:
         while(low <= high)
         {
             int mid = low + (high-low)/2;
+            // if target is present at middle return mid index
             if(nums[mid] == target)
                 return mid;
             // left side is sorted
             if(nums[low] <= nums[mid])
             {
+    //if target is in range from low to mid i.e in left half narrow down the search to left half only
+
                 if(target >= nums[low] && target <= nums[mid])
                     high = mid-1;
+    // target is not in the low to mid range explore the right half it may have the target value
+
                 else 
                     low = mid+1;
             }
+//right half is sorted 
             else
             {
+        //if target is in range from mid to high i.e right half narrow down the search to right half only
+
                 if(target >= nums[mid] && target <= nums[high])
                     low = mid+1;
+        //target is not in mid to high range explore left half it may have the target value
+
                 else
                     high = mid-1;
             }
         }
+    //element not present in the array at all
+
         return -1;
     }
 };
