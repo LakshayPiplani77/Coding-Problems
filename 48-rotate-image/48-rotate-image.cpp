af36fdb375750e4671e159657a16c3ad1d0f66b0
Lakshay Piplani <lakshaypiplani77@gmail.com>
2022-08-30 08:57:19 +0530

    Time: 3 ms (64.32%), Space: 7.1 MB (73.79%) - LeetHub

--- 48-rotate-image/48-rotate-image.cpp
+++ 48-rotate-image/48-rotate-image.cpp
@@ -3,6 +3,7 @@ public:
     void rotate(vector<vector<int>>& matrix) {
         
         int n = matrix.size();
+        //transpose the matrix
         for(int i = 0;i<n;i++)
         {
             for(int j = 0;j<i;j++)
@@ -10,6 +11,7 @@ public:
                 swap(matrix[i][j],matrix[j][i]);
             }
         }
+        //reverse every row
         for(int i = 0;i<n;i++)
         {
             reverse(matrix[i].begin(),matrix[i].end());
